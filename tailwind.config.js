1ee236a0f616ab6f704da14b39adaac440462f6b
tinong279 <tinong279@gmail.com>
2025-11-27 00:58:33 +0800

    第五頁

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 export default {
   content: ["./*.html", "./src/**/*.{js,html}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans TC", "sans-serif"],
+        serifEng: ["Cormorant Garamond", "serif"],
+        serifTC: ["Noto Serif TC", "serif"],
+        sansTC: ["Noto Sans TC", "sans-serif"],
+      },
+    },
+  },
   plugins: [],
 }
